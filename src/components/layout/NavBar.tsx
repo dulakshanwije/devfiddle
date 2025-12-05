a150871c7f09a1a3b44ab9333f1db510d1e2ebcc
Dulakshan Wijesundara <dulakshanwije@gmail.com>
2025-12-05 23:27:28 +0530

    UI Changes

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -1,70 +1,86 @@
-'use client'
+"use client";
 
-import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
-import Image from 'next/image'
-import Link from 'next/link'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
 
 const navigation = [
-    { name: 'Home', href: '#', current: true },
-    { name: 'Tools', href: '#', current: false },
-    { name: 'Propmts', href: '#', current: false },
-    { name: 'Articles', href: '#', current: false }
-]
+  { name: "Home", href: "#", current: true },
+  { name: "Tools", href: "#", current: false },
+  { name: "Propmts", href: "#", current: false },
+  { name: "Articles", href: "#", current: false },
+];
 
 const classNames = (...classes: string[]) => {
-    return classes.filter(Boolean).join(' ')
-}
+  return classes.filter(Boolean).join(" ");
+};
 
 const NavBar = () => {
-    return (
-        <Disclosure
-            as="nav"
-            className="relative bg-white drop-shadow-lg after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
-        >
-            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-                <div className="relative flex h-16 items-center justify-between">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        {/* Mobile menu button*/}
-                        <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-primary/10 focus:outline-2 focus:-outline-offset-1 focus:outline-primary">
-                            <span className="absolute -inset-0.5" />
-                            <span className="sr-only">Open main menu</span>
-                            <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                            <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-                        </DisclosureButton>
-                    </div>
-                    <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
-                        <div className="flex shrink-0 items-center">
-                            <Link href={"/"}>
-                                <Image
-                                    alt="DulaBuilds.Dev"
-                                    src="/images/logo.svg"
-                                    width={50}
-                                    height={100}
-                                    className="h-5 w-auto"
-                                    unoptimized
-                                />
-                            </Link>
-                        </div>
-                        <div className="hidden sm:ml-6 sm:block">
-                            <div className="flex space-x-4">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        aria-current={item.current ? 'page' : undefined}
-                                        className={classNames(
-                                            item.current ? 'bg-primary text-white' : 'text-black hover:bg-primary/10',
-                                            'rounded-md px-3 py-2 text-sm font-medium',
-                                        )}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                    {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+  return (
+    <Disclosure
+      as="nav"
+      className="relative bg-white drop-shadow-lg after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10"
+    >
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile menu button*/}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-black hover:bg-primary/10 focus:outline-2 focus:-outline-offset-1 focus:outline-primary">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block size-6 group-data-open:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden size-6 group-data-open:block"
+              />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
+            <div className="flex shrink-0 items-center">
+              <Link href={"/"}>
+                <Image
+                  alt="DulaBuilds.Dev"
+                  src="/images/logo.svg"
+                  width={50}
+                  height={100}
+                  className="h-5 w-auto"
+                  unoptimized
+                />
+              </Link>
+            </div>
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? "page" : undefined}
+                    className={classNames(
+                      item.current
+                        ? "bg-primary text-white"
+                        : "text-black hover:bg-primary/10",
+                      "rounded-md px-3 py-2 text-sm font-medium"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </div>
+          {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         <button
                             type="button"
                             className="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500"
@@ -115,29 +131,31 @@ const NavBar = () => {
                             </MenuItems>
                         </Menu>
                     </div> */}
-                </div>
-            </div>
+        </div>
+      </div>
 
-            <DisclosurePanel className="sm:hidden">
-                <div className="space-y-1 px-2 pt-2 pb-3">
-                    {navigation.map((item) => (
-                        <DisclosureButton
-                            key={item.name}
-                            as="a"
-                            href={item.href}
-                            aria-current={item.current ? 'page' : undefined}
-                            className={classNames(
-                                item.current ? 'bg-primary text-white' : 'text-black hover:bg-primary/10',
-                                'block rounded-md px-3 py-2 text-base font-medium',
-                            )}
-                        >
-                            {item.name}
-                        </DisclosureButton>
-                    ))}
-                </div>
-            </DisclosurePanel>
-        </Disclosure>
-    )
-}
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
+          {navigation.map((item) => (
+            <DisclosureButton
+              key={item.name}
+              as="a"
+              href={item.href}
+              aria-current={item.current ? "page" : undefined}
+              className={classNames(
+                item.current
+                  ? "bg-primary text-white"
+                  : "text-black hover:bg-primary/10",
+                "block rounded-md px-3 py-2 text-base font-medium"
+              )}
+            >
+              {item.name}
+            </DisclosureButton>
+          ))}
+        </div>
+      </DisclosurePanel>
+    </Disclosure>
+  );
+};
 
 export default NavBar;
--- src/components/ui/ToolCard.tsx
+++ src/components/ui/ToolCard.tsx
@@ -2,28 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 
 type ToolCardProps = {
-    title: string,
-    icon: string,
-    description: string,
-    link: string
+  title: string;
+  icon: string;
+  description: string;
+  link: string;
 };
 
 const ToolCard = ({ title, icon, description, link }: ToolCardProps) => {
-    return <>
-        <Link href={link}>
-            <div className="border border-primary w-full h-[180px] bg-white rounded-2xl drop-shadow-md flex flex-col justify-center items-center gap-2 hover:drop-shadow-lg hover:scale-101">
-                <Image
-                    src={icon}
-                    width={'80'}
-                    height={'80'}
-                    alt={'icon'}
-                    className="w-10 mb-1"
-                />
-                <h3 className="text-base font-bold leading-4.5 tracking-wide">{title}</h3>
-                <p className="max-w-3/4 text-xs text-center leading-4 text-mute">{description}</p>
-            </div>
-        </Link>
+  return (
+    <>
+      <Link href={link}>
+        <div className="border border-primary w-full h-[180px] bg-white rounded-2xl drop-shadow-md flex flex-col justify-start items-center gap-2 hover:drop-shadow-lg hover:scale-101">
+          <Image
+            src={icon}
+            width={"80"}
+            height={"80"}
+            alt={"icon"}
+            className="w-10 mb-1 pt-6"
+          />
+          <h3 className="text-base font-bold leading-4.5 tracking-wide text-center">
+            {title}
+          </h3>
+          <p className="max-w-3/4 text-xs text-center leading-4 text-mute">
+            {description}
+          </p>
+        </div>
+      </Link>
     </>
-}
+  );
+};
 
 export default ToolCard;
